b65e9f52508a8e95ed300359bafe354d2afedc14
sharkljk <1981894692@qq.com>
2019-11-05 16:28:52 +0800

    qwn

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/Ranking">Ranking</router-link>
+    <!-- 顶部 -->
+    <div class="top">
+      <div class="left"></div>
+      <div class="auto">TOPTOP</div>
+      <div class="right">
+        <van-icon name="search" />
+      </div>
     </div>
-    <router-view/>
+
+    <!-- 各页面 -->
+    <router-view  class="boxs"/>
+
+    <!-- 底部导航 -->
+    <van-tabbar v-model="active" class="btn">
+      <van-tabbar-item name="home" icon="home-o" @click="router('Home')"></van-tabbar-item>
+
+      <van-tabbar-item name="search" icon="search" @click="router('Find')"></van-tabbar-item>
+
+      <van-tabbar-item name="friends" icon="friends-o" @click="router('Ranking')"></van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o" @click="router('Personal')"></van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
-
-<style lang="less">
+<script>
+export default {
+  data() {
+    return {
+      active: ""
+    }
+  },
+  methods:{
+    router(lx){
+      this.$router.push({ path: lx, query: { lx: "157" } });
+    }
+  }
+};
+</script>
+<style  lang="less">
+.boxs{
+  margin-top: 1rem;
+}
+.btn {
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
+.top {
+  background: #f7f5fa;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 1rem;
+  z-index: 999;
+  // display: flex;
+  // justify-content: baseline;
+  
+  .left {
+    width: 1rem;
+    height: 100%;
+    background: red;
+    float: left;
+  }
+  .auto {
+    float: left;
+    font-size: 0.5rem;
+    width: 2rem;
+    margin: 0 auto;
+  }
+  .right {
+    float: right;
+    width: 1rem;
+    height: 100%;
+  }
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background: #f7f5fa;
 }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,73 @@
 <template>
-  <div>首页</div>
+  <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+  <p >刷新次数: {{ count }}</p>
+
+  <van-list
+  v-model="loading"
+  :finished="finished"
+  finished-text="没有更多了"
+  @load="onLoad"
+>
+  <van-cell
+    v-for="item in list"
+    :key="item"
+    :title="item"
+  />
+</van-list>
+
+</van-pull-refresh>
+
 </template>
 
 <script>
-export default {};
+// import { createNamespacedHelpers } from "vuex";
+// const { mapState, mapActions } = createNamespacedHelpers("shouye");
+export default {
+  data() {
+    return {
+      count: 0,
+      isLoading: false,
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+
+  methods: {
+    onRefresh() {
+      setTimeout(() => {
+        this.$toast('刷新成功');
+        this.isLoading = false;
+        this.count++;
+      }, 500);
+    },
+    onLoad() {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 500);
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped >
+p{
+    font-size: 0.5rem;
+}
+.van-pull-refresh{
+    z-index: 999;}
+.box{
+    height: 6rem;
+    background: lightcoral;
+}
 </style>
